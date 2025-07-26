468025c63957d9d6af4e9878135c64d53f8e94c5
tokumeiDragon <tototototokumei@gmail.com>
2025-07-26 13:21:35 +0900

    すすめた

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  */
 import 'dotenv/config';
 const ENV = process.env;
-const MODE = process.argv[2] || `PRD`;
+const MODE = process.argv[2] || `DEV`;
 
 /**
  * 外部ライブラリ読込
@@ -23,28 +23,32 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
-	],
+	]
 });
 
 /**
  * クライアントが準備完了時のイベント
  */
 client.on(Events.ClientReady, async (client) => {
-	console.log(`${client.user.username}が起動したよ！`);
+	if (MODE !== `DEV`) console.log(`${client.user.username}が起動したよ！`);
 });
 
 /**
  * メッセージ受信時のイベント
  */
 
-client.on(Events.MessageCreate, async (msg) => {
-	// Bot やシステム系は除外
-	if (!msg.inGuild() || msg.author.bot || msg.system) return;
+client.on(Events.MessageCreate, async (message) => {
 
-	const ch = msg.channel;
-	if (ch.isSendable()) {
-		await ch.send(`受信: ${msg.content}`);
-	}
+	// ガード節
+	const endConditions = [
+		!message.inGuild(),
+		message.author.bot,
+		message.system,
+	];
+	if (endConditions.some(e => e)) return;
+
+	const channel = message.channel;
+	if (channel.isSendable()) await channel.send(`受信: ${message.content}`);
 });
 
 /**
